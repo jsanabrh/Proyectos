18922edf0e7ab56abc374ad8af2a99868ecf74b2
Jp Gomez <jpgommez@gmail.com>
2022-04-07 07:17:50 -0500

    Nuevo envio

---
+++ src/momento1moviles1/Momento1Moviles1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package momento1moviles1;
+
+/**
+ *
+ * @author 507
+ */
+public class Momento1Moviles1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
